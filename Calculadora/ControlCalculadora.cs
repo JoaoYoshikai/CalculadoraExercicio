3044a936d51a5bd7107c85995b2466b23791ae4b
JOAO PAULO YOSHIKAI PAZINATO CRUZ <joao.pypcruz@senacsp.edu.br>
2023-07-27 13:45:57 -0300

    Commit exercicio

---
+++ Calculadora/ControlCalculadora.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+     class ControlCalculadora
+    {
+        //Criando um objeto calculadora na memória
+        ModelCalculadora calculadora;
+
+        public ControlCalculadora() 
+        {
+            this.calculadora = new ModelCalculadora();
+        }//Fim do construtor
+
+        public void ColetarBinario()
+        {
+            Console.WriteLine("Informe o número binário:");
+            this.calculadora.GetSetBinario = Convert.ToString(Console.ReadLine());
+        }//Fim do coletar binário
+        
+
+        public void Coletar()
+        {
+            Console.WriteLine("Informe um número!");
+            this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe outro número!");
+            this.calculadora.GetSetNum2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe mais um número!");
+            this.calculadora.GetSetNum3 = Convert.ToDouble(Console.ReadLine());
+
+
+        }//Fim do coletar
+
+        public int menu()
+        {
+            Console.WriteLine("-----MENU-----"   +
+                              "\n0. Sair" +  
+                              "\n1. Somar"       +
+                              "\n2. Subtrair"    +
+                              "\n3. Dividir"     +
+                              "\n4. Multiplicar" +
+                              "\n5. Potência "   +
+                              "\n6. Raiz Quadrada " +  
+                              "\n7. Tabuada" +
+                              "\n8. Bhaskara" +
+                              "\n9. Binário - Decimal" +
+                              "\n\nEscolha uma das opções acima : ");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            return opcao;
+        }//Fim do método menu
+
+        public void Operacao()
+        {
+            int opcao = 0;
+            do
+            {
+                opcao = menu();
+                Console.Clear();//Limpa tela
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+
+                    case 1:
+                        Coletar();
+                        Console.WriteLine("A soma: " + this.calculadora.Somar());
+                        break;
+                    case 2:
+                        Coletar();
+                        Console.WriteLine("A subtração: " + this.calculadora.Somar());
+                        break;
+                    case 3:
+                        Coletar();
+                        if (this.calculadora.Dividir() == -1)
+                        {
+                            Console.WriteLine("Impossível dividir por zero");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Divisão:" + this.calculadora.Dividir());
+                        }//Fim do if
+                        break;
+                    case 4:
+                        Coletar();
+                        Console.WriteLine("Multiplicar:" + this.calculadora.Multiplicar());
+                        break;
+                    case 5:
+                        Coletar();
+                        Console.WriteLine("Potência:" + this.calculadora.Multiplicar());
+                        break;
+                    case 6:
+                        Coletar();
+                        Console.WriteLine("Raiz:" + this.calculadora.Multiplicar());
+                        break;
+                    case 7:
+                        Console.WriteLine("Informe um número: ");
+                        this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(this.calculadora.TabuadaNum1());
+                        break;
+
+                    case 8:
+                        Coletar();
+                        Console.WriteLine(this.calculadora.Bhaskara());
+                        break;
+                    case 9:
+                        ColetarBinario();
+                        Console.WriteLine(this.calculadora.ConverterParaDecimal());
+                        break;
+                    default:
+                        Console.WriteLine("Opção escolhida nao é válida");
+                        break;
+                
+                
+                }//Fim do switch
+            } while (opcao != 0); //Fim do Do While 
+        }//Fim metodo operacao
+
+
+
+
+
+    }//Fim da classe
+}//Fim do projeto
---
+++ Calculadora/ModelCalculadora.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class ModelCalculadora
+    {
+        //Declarando variáveis
+        private double num1;
+        private double num2;
+        private double num3;
+        private string binario;
+
+
+        public ModelCalculadora()
+        {
+            GetSetNum1 = 0;
+            GetSetNum2 = 0;
+            GetSetNum3 = 0;
+            GetSetBinario = "";
+        }//Fim do construtor
+
+        //Métodos gets e sets
+
+        public double GetSetNum1
+        {
+            get { return this.num1; }
+            set { this.num1 = value; }
+        }//Fim do GetSetNum1
+
+        public double GetSetNum2
+        {
+            get { return this.num2; }
+            set { this.num2 = value; }
+        }//Fim do GetSetNum2
+
+        public double GetSetNum3
+        {
+            get { return this.num3; }
+            set { this.num3 = value; }
+        }//Fim do GetSetNum2
+
+        public string GetSetBinario
+        {
+            get { return this.binario; }
+            set { this.binario = value; }
+        }//Fim do GetSetNum2
+
+
+
+        //Métodos
+
+        public double Somar()
+        {
+            return GetSetNum1 + GetSetNum2;
+        }//Fim do método somar
+
+        public double Subtrair()
+        {
+            return GetSetNum1 - GetSetNum2;
+        }//Fim do método subtrair
+
+        public double Dividir()
+        {
+            if (GetSetNum2 <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return GetSetNum1 / GetSetNum2;
+            }
+        }//Fim do método dividir
+
+        public double Multiplicar()
+        {
+            return GetSetNum1 * GetSetNum2;
+        }//Fim do método multiplicar
+
+        public double Potencia()
+        {
+            return Math.Pow(GetSetNum1, GetSetNum2);
+        }//Fim da potência
+
+        public string Raiz()
+        {
+            string msg = "Raiz do primeiro número:" + Math.Sqrt(GetSetNum1) +
+                       "\nRaiz do segundo número:" + Math.Sqrt(GetSetNum2);
+            return msg;
+        }//Fim do método Raiz
+
+        public string TabuadaNum1()
+        {
+            string resultado = "";
+            for (int i = 0 ; i <= 10; i++)
+                {
+                resultado += "\n" + GetSetNum1 + " * " + i + " = " + (GetSetNum1 * i);
+                }//Fim do For
+            return resultado;
+        }//Fim do método Tabuada1
+
+
+        public string TabuadaNum2()
+        {
+            string resultado = "";
+            for (int i = 0; i <= 10; i++)
+            {
+                resultado += "\n" + GetSetNum2 + " * " + i + " = " + (GetSetNum2 * i);
+            }//Fim do For
+            return resultado;
+        }//Fim do método Tabuada2
+
+        public string Bhaskara()
+        {
+
+            
+            string msg = "X1 de delta:" + (-GetSetNum2 + (Math.Sqrt(Math.Pow(GetSetNum2, 2) - (4 * GetSetNum1 * GetSetNum3)))) / (2 * GetSetNum1)
+                      +"\nX2 de delta:" + (-GetSetNum2 - (Math.Sqrt(Math.Pow(GetSetNum2, 2) - (4 * GetSetNum1 * GetSetNum3)))) / (2 * GetSetNum1);
+
+            return msg;
+        }//fim do metodo Bhaskara
+
+        public string ConverterParaDecimal()
+        {
+            int i = 0;
+            string dec = "0";
+
+            for( i = 0; i < GetSetBinario.Length; i++)
+            { 
+                if (GetSetBinario.Substring(i, 1) == "1")
+            {
+                dec += Math.Pow(Convert.ToDouble(GetSetBinario), i); 
+            }//Fim do if
+            }//Fim do for
+
+            return dec;
+        }//Fim ConverterParaDecimal
+
+
+
+
+
+
+
+
+
+
+    }//Fim da classe
+}//Fim do projeto
